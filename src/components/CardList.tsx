1c5013e51a4c24710cfa2fd6a27653697fc3e958
Ednan <ednanjhony@yahoo.com.br>
2021-06-07 07:45:41 -0300

    added modal

--- src/components/CardList.tsx
+++ src/components/CardList.tsx
@@ -1,4 +1,5 @@
 import { SimpleGrid, useDisclosure } from '@chakra-ui/react';
+
 import { useState } from 'react';
 import { Card } from './Card';
 import { ModalViewImage } from './Modal/ViewImage';
@@ -17,16 +18,24 @@ interface CardsProps {
 
 export function CardList({ cards }: CardsProps): JSX.Element {
   // TODO MODAL USEDISCLOSURE
-
+  const { onOpen, isOpen, onClose } = useDisclosure();
   // TODO SELECTED IMAGE URL STATE
-
+  const [thisImage, setThisImage] = useState('');
   // TODO FUNCTION HANDLE VIEW IMAGE
+  const handleViewImage = (url: string): void => {
+    setThisImage(url);
+    onOpen();
+  };
 
   return (
     <>
-      {/* TODO CARD GRID */}
+      <SimpleGrid columns={3} spacing="40px">
+        {cards.map(card => (
+          <Card key={card.id} data={card} viewImage={handleViewImage} />
+        ))}
+      </SimpleGrid>
 
-      {/* TODO MODALVIEWIMAGE */}
+      <ModalViewImage isOpen={isOpen} onClose={onClose} imgUrl={thisImage} />
     </>
   );
 }
--- src/components/Modal/ViewImage.tsx
+++ src/components/Modal/ViewImage.tsx
@@ -20,4 +20,17 @@ export function ModalViewImage({
   imgUrl,
 }: ModalViewImageProps): JSX.Element {
   // TODO MODAL WITH IMAGE AND EXTERNAL LINK
+  return (
+    <Modal isOpen={isOpen} onClose={onClose}>
+      <ModalOverlay maxW="900px" maxH="600px" h="100vh" />
+      <ModalContent p={0}>
+        <ModalBody>
+          <Image src={imgUrl} w="100%" h="100%" />
+        </ModalBody>
+        <ModalFooter bg="pGray.900" display="flex" justifyContent="flex-start">
+          <Link href={imgUrl}>Abrir</Link>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+  );
 }
